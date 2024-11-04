0a9f39ed0f0c7de07da8e366c4f9cb8b52764ccf
Jonathan Lavén <jonathan.laven123@gmail.com>
2024-11-04 19:39:30 +0100

    add comments

--- src/graphics.c
+++ src/graphics.c
@@ -2,18 +2,16 @@
 
 
 int main(){
-    volatile char *VGA = (volatile char*) 0x08000000;
+    volatile char *VGA = (volatile char*) 0x08000000; // Create a pointer to the VGA pixel buffer. This is the "drawing" area.
     for (int i = 0; i < 320*480; i++)
-        VGA[i] = i / 320;
+        VGA[i] = i / 320; // Fill the drawing area with some values.
     unsigned int y_ofs= 0;
-    volatile int *VGA_CTRL = (volatile int*) 0x04000100;
+    volatile int *VGA_CTRL = (volatile int*) 0x04000100; // Create a pointer to the VGA DMA
     while (1){
-        *(VGA_CTRL+1) = (unsigned int) (VGA+y_ofs*320);
-        *(VGA_CTRL+0) = 0;
-       
-        y_ofs= (y_ofs+ 1) % 240;
-        
+        *(VGA_CTRL+1) = (unsigned int) (VGA+y_ofs*320); // Update the backbuffer to point to the VGA pixel buffer + 320*y_ofs
+        *(VGA_CTRL+0) = 0; // Write to the backbuffer control register to perform the swap.
+        y_ofs= (y_ofs+ 1) % 240; // Increase y_ofs by one and wrap around when reaching 240.
         for (int i = 0; i < 1000000; i++)
-            asm volatile ("nop");
+            asm volatile ("nop"); // Delay for some unit of time
     }
 }
